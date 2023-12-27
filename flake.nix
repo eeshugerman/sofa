2b635af49fa062ba9fe40baed0fd13d1a1cad3b4
Elliott Shugerman <eeshugerman@gmail.com>
2023-12-26 22:44:31 -0500

    initial commit (dump from postgres-backup-s3 repo)

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ janet jpm ];
+          # needed for jpm to build janet executables (like jfmt)
+          JANET_LIBPATH = nixpkgs.lib.makeLibraryPath [ pkgs.janet ];
+        };
+      });
+}
---
+++ project.janet
@@ -0,0 +1,14 @@
+(declare-project
+ :name "sofa"
+ :author "Elliott Shugerman <eeshugerman@gmail.com>"
+ :description "A testing library, insired by Mocha"
+ :license "MIT"
+ :url "https://github.com/eeshugerman/sofa"
+ :repo "git+https://github.com/eeshugerman/sofa"
+ :dependencies ["https://github.com/janet-lang/spork"])
+
+(declare-source
+ :prefix "sofa"
+ :source ["src/sofa.janet"])
+
+# TODO declare-binsrcipt? https://github.com/ianthehenry/judge/blob/master/project.janet#L25
